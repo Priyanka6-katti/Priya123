9613580bb54a77a81be6197fe66b53486c04e37e
Priya <Priya@Priya-PC>
2020-07-21 14:57:21 +0530

    Design

---
+++ src/bhimu/Pri1.java
@@ -0,0 +1,11 @@
+package bhimu;
+
+public class Pri1 {
+
+	public static void main(String[] args) {
+		
+          System.out.println("Hi hello");
+          System.out.println("Hi hello whatsup");
+	}
+
+}
---
+++ src/bhimu/Pri2.java
@@ -0,0 +1,11 @@
+package bhimu;
+
+public class Pri2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I am good");
+       System.out.println("going goood");
+	}
+
+}
